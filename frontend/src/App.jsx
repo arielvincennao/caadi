43d0fda3e17bf8698854cb15a8e39d9b33ae3b4d
aguslorenzo <aguslorenzo21@gmail.com>
2025-11-23 10:30:19 -0300

    added map component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Menu from './pages/Menu'
+import MapView from './components/Map/MapView'
 
 function App() {
   return (
