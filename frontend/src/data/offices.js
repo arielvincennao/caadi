eb2c7ef9373b6244aa7a55cb17929c594e7e4d51
aguslorenzo <aguslorenzo21@gmail.com>
2025-11-27 15:00:11 -0300

    added modal and office data

---
+++ frontend/src/data/offices.js
@@ -0,0 +1,18 @@
+export const offices = [
+  {
+    id: 1,
+    institution: "Municipalidad de Tandil",
+    address: "Belgrano 485, Tandil",
+    contact: "2494 444000",
+    schedule: "Lunes a Viernes 8:00 a 14:00",
+    geocode: [-37.32967013550194, -59.13681692154323]
+  },
+  {
+    id: 2,
+    institution: "Hospital Ramón Santamarina",
+    address: "Av. España 851, Tandil",
+    contact: "2494 444200",
+    schedule: "24 horas",
+    geocode: [-37.3152552168332, -59.13879327394908]
+  }
+];
