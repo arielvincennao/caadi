38248c7de34a7a3d2621e4c651f4226395840adc
Ale Guay <galeguay@gmail.com>
2025-11-29 11:22:26 -0300

    Se agregó BtnBack y NavBar al componente Section

--- frontend/src/pages/Section.jsx
+++ frontend/src/pages/Section.jsx
@@ -1,7 +1,8 @@
 /* import Steps from {}
 import Map from {}
-import BtnLink from {}
-import Btn from {} */
+import BtnLink from {} */
+import BtnBack from "../components/common/BtnBack";
+import Navbar from "../components/layout/Navbar";
 import Step from "../components/sections/Step";
 import { Title } from "../components/Typography";
 import { Text } from "../components/Typography";
@@ -11,46 +12,25 @@ export default function Section({
 }) {
     if (!data) return null;
 
-    /*   const bloques = {
-        map: Map,
-        steps: Steps,
-        btn: Btn,
-        btn_link: BtnLink
-        }; */
-    const blocks = {
-        steps: Step,
-    }
-
     return (
         <div className="bg-white min-h-screen flex flex-col items-center text-lg md:text-2xl">
+            <Navbar/>
+            <div className="absolute top-23 left-4 md:top-25 md:left-10 z-10">
+                <BtnBack />
+            </div>
             <div className="p-3 md:w-[70%]">
-                <section className="flex flex-col items-center text-center mb-8">
+                <section className="flex flex-col mb-8">
                     <img
                         src={data.image}
                         alt="Imagen de Portada"
                         className="w-full max-h-64 object-cover rounded-lg mb-6"
                     />
-                    <Title>{data.name}</Title>
+                    <Title className="text-center mb-3">{data.name}</Title>
                     <Text>{data.description}</Text>
                 </section>
 
-                {/*{data.contentBlocks
-            .sort((a, b) => a.order - b.order)
-            .map((block) => {
-                const Componente = bloques[block.type];
-
-                if (!Componente) {
-                console.warn(`No existe componente para el tipo: ${block.type}`, block);
-                return null;
-                }
-
-                return (
-                    <section key={b.id}>
-                        <Componente data={b} />
-                    </section>
-                );
-            })} */}
                 {data.contentBlocks.map((block) => {
+
                     if (block.type === "steps") {
                         return (
                             <section key={block.id} className="my-6 space-y-4">
@@ -65,6 +45,8 @@ export default function Section({
                             </section>
                         );
                     }
+
+                    
                     return null;
                 })}
                 
