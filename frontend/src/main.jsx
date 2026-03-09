645f78d2f65ae5058898de4ef612459a4bbdee80
aguslorenzo <aguslorenzo21@gmail.com>
2026-03-09 16:44:50 -0300

    elimine el window para probar desde consola

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.css';
 import App from './App.jsx'
-import { OfficeService } from './api/services/OfficeService.js'
-window.OfficeService = OfficeService
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
